0eba578879add9e7353165e7edb0bbc83c72e983
zoemhart <zoemhrt@gmail.com>
2017-05-17 20:27:35 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Iosevka', Helvetica, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+a {
+  color: #6190A6 !important;
+}
+</style>
